5a316360a05473f5690668dcdce63ae776850d94
Hanu Engelbrecht <engelbrecht.hanu@gmail.com>
2023-10-19 10:38:38 +0200

    Job & JobDescription added

---
+++ server/src/entity/Job.ts
@@ -0,0 +1,40 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { Problem } from "./Problem"; // Import the Problem entity
+import { Employee } from "./Employee"; // Import the Employee entity
+
+@Entity({ name: "Job" })
+export class Job {
+
+    @PrimaryGeneratedColumn()
+    JobId!: number;
+
+    @Column({ type: "int" })
+    ProblemId!: number;
+
+    @Column({ type: "int" })
+    EmpId!: number;
+
+    @Column({ type: "int" })
+    CreatedBy!: number;
+
+    @Column({ type: "varchar", length: 255 })
+    Description!: string;
+
+    @Column({ type: "date" })
+    Start!: Date;
+
+    @Column({ type: "date", nullable: true })
+    End!: Date | null;
+
+    @ManyToOne(() => Problem)
+    @JoinColumn({ name: "ProblemId" })
+    Problem!: Problem;
+
+    @ManyToOne(() => Employee, { onDelete: "CASCADE" }) // onDelete: "CASCADE" is used to automatically delete the job when the associated employee is deleted
+    @JoinColumn({ name: "EmpId" })
+    Employee!: Employee;
+
+    @ManyToOne(() => Employee, { onDelete: "CASCADE" }) // onDelete: "CASCADE" is used to automatically delete the job when the created employee is deleted
+    @JoinColumn({ name: "CreatedBy" })
+    CreatedByEmployee!: Employee;
+}
